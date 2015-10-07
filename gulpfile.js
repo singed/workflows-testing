16b13be53a71438a4080bf5501819879fdc17471
Alexander Parfenchuk <parfenchuks@gmail.com>
2015-10-07 21:37:31 +0300

    compass

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
 	gulputil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/*.coffee'];
@@ -11,6 +12,7 @@ var jsSources = [
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -25,3 +27,14 @@ gulp.task('js', function(){
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'));
 });
+
+gulp.task('compass', function(){
+  gulp.src(sassSources)
+    .pipe(compass({
+    	sass:'components/sass',
+    	image:'builds/development/images',
+    	style:'expanded'
+    }))
+    .on('error', gulputil.log)
+    .pipe(gulp.dest('builds/development/css'));
+})
